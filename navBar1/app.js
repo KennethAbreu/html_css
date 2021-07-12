0a7e8cf9bf5a8141296bef8b6155122cb966332b
Kenneth <Kennethabreu1@gmail.com>
2021-07-12 18:34:28 -0400

    Completed Tutorial, still need readme

--- navBar1/app.js
+++ navBar1/app.js
@@ -2,16 +2,27 @@ const navSlide = () => {
     const burger = document.querySelector('.burger'); /*-- selecting the .burger <div> from html --*/
     const nav = document.querySelector('.nav-links'); 
     const navLinks = document.querySelectorAll('.nav-links li');
-//TOGGLE NAV
+
+
+
+    //TOGGLE NAV
     burger.addEventListener('click', () => {
         nav.classList.toggle('nav-active');
 
-    });
-    //ANIMATE LINKS IN SIDE MENU MOBILE BAR
+           //ANIMATE LINKS IN SIDE MENU MOBILE BAR
     navLinks.forEach((link, index) => {
-        link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 1.5}s`;
+        if(link.style.animation) {
+            link.style.animation= ''
+        } else {
+         link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`;
+        }
+     });
+
+     //BURGER ANIMATION
+     burger.classList.toggle('toggle');
+
     });
+ 
 }
 
 navSlide();
-//test comment//
